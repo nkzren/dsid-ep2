d42faa5db7bbdace8f6b7ef0303e242e5ac91061
Renan Nakazawa <renan.nakazawa0@gmail.com>
2021-07-18 20:02:23 -0300

    inicia a implementacao de media

--- aggregation-functions/src/main/java/org/cansados/aggregations/AverageAggregator.java
+++ aggregation-functions/src/main/java/org/cansados/aggregations/AverageAggregator.java
@@ -1,5 +1,7 @@
 package org.cansados.aggregations;
 
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class AverageAggregator {
@@ -9,5 +11,14 @@ public class AverageAggregator {
                 .builder()
                 .appName("JavaWordCount")
                 .getOrCreate();
+        spark.log().info("Starting spark app...");
+
+        for (String arg : args) {
+            spark.log().info("will download csv from: " + arg);
+        }
+
+        JavaRDD<Row> lines = spark.read().csv(args).javaRDD();
+
+        System.out.println("number of lines read:" + lines.count());
     }
 }
