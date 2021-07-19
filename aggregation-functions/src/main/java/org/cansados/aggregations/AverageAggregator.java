7c8cb2bc84bb862b5c926c32d283675bd3ee2632
Renan Nakazawa <renan.nakazawa0@gmail.com>
2021-07-19 20:58:18 -0300

    implementa medias de temperatura

--- aggregation-functions/src/main/java/org/cansados/aggregations/AverageAggregator.java
+++ aggregation-functions/src/main/java/org/cansados/aggregations/AverageAggregator.java
@@ -1,14 +1,16 @@
 package org.cansados.aggregations;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AverageAggregator {
     public static void main(String[] args) {
@@ -30,8 +32,34 @@ public class AverageAggregator {
             spark.log().info("will download csv from: " + arg);
         }
 
-        Dataset<Row> lines = spark.read().csv(argList.toArray(new String[0]));
+        JavaRDD<Row> lines = spark.read().format("csv")
+                .option("sep", ",")
+                .option("inferSchema", "true")
+                .option("header", "true")
+                .load(argList.toArray(new String[0]))
+                .toJavaRDD();
 
-        spark.log().info("number of lines read:" + lines.count());
+        // Group by year
+        JavaPairRDD<Integer, Iterable<Row>> byYear = lines.groupBy(row -> {
+            String dateString = row.getAs("Date");
+            return LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd")).getYear();
+        });
+
+        List<Tuple2<Integer, Double>> meanTempByYear = byYear.aggregateByKey(new Tuple2<>(0, 0.0),
+                (tuple, rows) -> {
+                    AtomicReference<Integer> count = new AtomicReference<>(0);
+                    AtomicReference<Double> sum = new AtomicReference<>(0.0);
+                    rows.iterator().forEachRemaining(row -> {
+                        Double current = row.getAs("Mean_Temp");
+                        sum.updateAndGet(v -> v + current);
+                        count.updateAndGet(v -> v + 1);
+                    });
+                    return new Tuple2<>(count.get(), sum.get());
+                },
+                (v1, v2) -> new Tuple2<>(v1._1 + v2._1, v1._2 + v2._2)
+        ).mapValues(sum -> { return (1.0 * sum._2 / sum._1); }).collect();
+
+        meanTempByYear.forEach(tuple -> spark.log().info("Year: "+ tuple._1 + " | Mean temperature: " + tuple._2));
+
     }
 }
