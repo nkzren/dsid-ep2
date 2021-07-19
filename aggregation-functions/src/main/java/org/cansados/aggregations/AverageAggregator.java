614ec279d4182751a8728cf81b314a762d59b1a1
Renan Nakazawa <renan.nakazawa0@gmail.com>
2021-07-18 21:31:38 -0300

    adiciona as credenciais da aws no jar do spark

--- aggregation-functions/src/main/java/org/cansados/aggregations/AverageAggregator.java
+++ aggregation-functions/src/main/java/org/cansados/aggregations/AverageAggregator.java
@@ -1,24 +1,37 @@
 package org.cansados.aggregations;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AverageAggregator {
     public static void main(String[] args) {
+        List<String> argList = new ArrayList<>(Arrays.asList(args));
 
         SparkSession spark = SparkSession
                 .builder()
                 .appName("JavaWordCount")
                 .getOrCreate();
+
+        Configuration config = spark.sparkContext().hadoopConfiguration();
+        config.set("fs.s3a.access.key", argList.remove(0));
+        config.set("fs.s3a.secret.key", argList.remove(0));
+        config.set("fs.s3a.endpoint", "s3.amazonaws.com");
+
         spark.log().info("Starting spark app...");
 
-        for (String arg : args) {
+        for (String arg : argList) {
             spark.log().info("will download csv from: " + arg);
         }
 
-        JavaRDD<Row> lines = spark.read().csv(args).javaRDD();
+        Dataset<Row> lines = spark.read().csv(argList.toArray(new String[0]));
 
-        System.out.println("number of lines read:" + lines.count());
+        spark.log().info("number of lines read:" + lines.count());
     }
 }
