3f45ce5deb8b2d7de3602d0b9c5b4b1c185c2567
Renan Nakazawa <renan.nakazawa0@gmail.com>
2021-07-19 22:26:04 -0300

    adiciona tratamento de erros basico no agregador de medias

--- aggregation-functions/src/main/java/org/cansados/aggregations/AverageAggregator.java
+++ aggregation-functions/src/main/java/org/cansados/aggregations/AverageAggregator.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class AverageAggregator {
     public static void main(String[] args) {
@@ -38,46 +39,53 @@ public class AverageAggregator {
             spark.log().info("will download csv from: " + arg);
         }
 
-        JavaRDD<Row> lines = spark.read().format("csv")
-                .option("sep", ",")
-                .option("inferSchema", "true")
-                .option("header", "true")
-                .load(argList.toArray(new String[0]))
-                .toJavaRDD();
+        try {
+            JavaRDD<Row> lines = spark.read().format("csv")
+                    .option("sep", ",")
+                    .option("inferSchema", "true")
+                    .option("header", "true")
+                    .load(argList.stream().filter(it -> it.startsWith("s3a")).toArray(String[]::new))
+                    .toJavaRDD();
 
-        // Group by year
-        JavaPairRDD<Integer, Iterable<Row>> byYear = lines.groupBy(row -> {
-            String dateString = row.getAs("Date");
-            return LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd")).getYear();
-        });
+            // Group by year
+            JavaPairRDD<Integer, Iterable<Row>> byYear = lines.groupBy(row -> {
+                String dateString = row.getAs("Date");
+                return LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd")).getYear();
+            });
 
-        JavaPairRDD<Integer, Double> meanTempByYear = byYear.aggregateByKey(new Tuple2<>(0, 0.0),
-                (tuple, rows) -> {
-                    AtomicReference<Integer> count = new AtomicReference<>(0);
-                    AtomicReference<Double> sum = new AtomicReference<>(0.0);
-                    rows.iterator().forEachRemaining(row -> {
-                        Double current = row.getAs("Mean_Temp");
-                        sum.updateAndGet(v -> v + current);
-                        count.updateAndGet(v -> v + 1);
-                    });
-                    return new Tuple2<>(count.get(), sum.get());
-                },
-                (v1, v2) -> new Tuple2<>(v1._1 + v2._1, v1._2 + v2._2)
-        ).mapValues(sum -> { return (1.0 * sum._2 / sum._1); });
+            JavaPairRDD<Integer, Double> meanTempByYear = byYear.aggregateByKey(new Tuple2<>(0, 0.0),
+                    (tuple, rows) -> {
+                        AtomicReference<Integer> count = new AtomicReference<>(0);
+                        AtomicReference<Double> sum = new AtomicReference<>(0.0);
+                        rows.iterator().forEachRemaining(row -> {
+                            Double current = row.getAs("Mean_Temp");
+                            sum.updateAndGet(v -> v + current);
+                            count.updateAndGet(v -> v + 1);
+                        });
+                        return new Tuple2<>(count.get(), sum.get());
+                    },
+                    (v1, v2) -> new Tuple2<>(v1._1 + v2._1, v1._2 + v2._2)
+            ).mapValues(sum -> { return (1.0 * sum._2 / sum._1); });
 
-        // Map results to
-        JavaRDD<Document> documents = meanTempByYear.map(tuple -> {
-            Map<String, String> valuesMap = Map.of(
-                    "inventoryId", inventoryId,
-                    "year", tuple._1.toString(),
-                    "avg", tuple._2.toString()
-            );
-            StringSubstitutor substitutor = new StringSubstitutor(valuesMap);
-            String templateString = "{ inventoryId: '${inventoryId}', year: '${year}', avg: '${avg}' }";
-            return Document.parse(substitutor.replace(templateString));
-        });
+            // Map results to
+            JavaRDD<Document> documents = meanTempByYear.map(tuple -> {
+                Map<String, String> valuesMap = Map.of(
+                        "inventoryId", inventoryId,
+                        "year", tuple._1.toString(),
+                        "avg", tuple._2.toString()
+                );
+                StringSubstitutor substitutor = new StringSubstitutor(valuesMap);
+                String templateString = "{ inventoryId: '${inventoryId}', year: ${year}, avg: ${avg} }";
+                return Document.parse(substitutor.replace(templateString));
+            });
 
-        MongoSpark.save(documents);
+            MongoSpark.save(documents);
+        } catch (Exception e) {
+            spark.log().error("Spark aggregation function threw an error. Listing args below: ");
+            // Remove aws credentials
+            argList.stream().filter(it -> !it.startsWith("fs.s3a")).forEachOrdered(arg -> spark.log().error(arg));
+            e.printStackTrace();
+        }
 
     }
 }
