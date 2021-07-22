4a51c25928b6c4c3c3a6522135fb85d4e3b31542
Renan Nakazawa <renan.nakazawa0@gmail.com>
2021-07-21 23:03:52 -0300

    refactor JavaRDDs para datasets + implementa stdev

--- aggregation-functions/src/main/java/org/cansados/aggregations/StdevAggregatorByMonth.java
+++ aggregation-functions/src/main/java/org/cansados/aggregations/StdevAggregatorByMonth.java
@@ -1,20 +1,12 @@
 package org.cansados.aggregations;
 
 import com.mongodb.spark.MongoSpark;
-import org.apache.commons.text.StringSubstitutor;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.bson.Document;
+import org.apache.spark.sql.*;
 import org.cansados.util.Util;
-import scala.Tuple2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class StdevAggregatorByMonth {
     public static void main(String[] args) {
@@ -22,8 +14,8 @@ public class StdevAggregatorByMonth {
 
         SparkSession session = Util.setupSparkSession(
                 argList,
-                "AverageAggregatorByMonth",
-                "averages"
+                "StdevAggregatorByMonth",
+                "stdev"
         );
 
         String inventoryId = argList.remove(0);
@@ -33,48 +25,21 @@ public class StdevAggregatorByMonth {
         Util.logFilePaths(session, argList);
 
         try {
-            JavaRDD<Row> lines = session.read().format("csv")
+            Dataset<Row> lines = session.read().format("csv")
                     .option("sep", ",")
                     .option("inferSchema", "true")
                     .option("header", "true")
-                    .load(argList.stream().filter(it -> it.startsWith("s3a")).toArray(String[]::new))
-                    .toJavaRDD();
+                    .load(argList.stream().filter(it -> it.startsWith("s3a")).toArray(String[]::new));
 
-            // Group by month
-            JavaPairRDD<String, Iterable<Row>> byMonth = lines.groupBy(row -> {
-                String month = row.getAs("Month");
-                String year = row.getAs("Year");
-                return year + "/" + month;
-            });
-
-            JavaPairRDD<String, Double> meanTempByMonth = byMonth.aggregateByKey(new Tuple2<>(0, 0.0),
-                    (tuple, rows) -> {
-                        AtomicReference<Integer> count = new AtomicReference<>(0);
-                        AtomicReference<Double> sum = new AtomicReference<>(0.0);
-                        rows.iterator().forEachRemaining(row -> {
-                            Double current = row.getAs(columnName);
-                            sum.updateAndGet(v -> v + current);
-                            count.updateAndGet(v -> v + 1);
-                        });
-                        return new Tuple2<>(count.get(), sum.get());
-                    },
-                    (v1, v2) -> new Tuple2<>(v1._1 + v2._1, v1._2 + v2._2)
-            ).mapValues(sum -> (1.0 * sum._2 / sum._1));
-
-            // Map results to
-            JavaRDD<Document> documents = meanTempByMonth.map(tuple -> {
-                Map<String, String> valuesMap = Map.of(
-                        "inventoryId", inventoryId,
-                        "groupedBy", "month",
-                        "label", tuple._1,
-                        "avg", tuple._2.toString()
-                );
-                StringSubstitutor substitutor = new StringSubstitutor(valuesMap);
-                String templateString = "{ inventoryId: '${inventoryId}', groupedBy: 'month', label: '${label}', avg: ${avg} }";
-                return Document.parse(substitutor.replace(templateString));
-            });
-
-            MongoSpark.save(documents);
+            MongoSpark.save(
+                lines.groupBy(
+                        new Column("Year").as("year"),
+                        new Column("Month").as("month"),
+                        new Column("ID").as("inventoryId")
+                )
+                .agg(functions.stddev(columnName).as("stdev"))
+                .orderBy("year", "month")
+            );
         } catch (Exception e) {
             session.log().error("Spark aggregation function threw an error. Listing args below: ");
             // Remove aws credentials
--- aggregation-functions/src/main/java/org/cansados/aggregations/StdevAggregatorByYear.java
+++ aggregation-functions/src/main/java/org/cansados/aggregations/StdevAggregatorByYear.java
@@ -1,22 +1,17 @@
 package org.cansados.aggregations;
 
 import com.mongodb.spark.MongoSpark;
-import org.apache.commons.text.StringSubstitutor;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.bson.Document;
 import org.cansados.util.Util;
-import scala.Tuple2;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import org.apache.spark.sql.functions;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class StdevAggregatorByYear {
     public static void main(String[] args) {
@@ -24,8 +19,8 @@ public class StdevAggregatorByYear {
 
         SparkSession session = Util.setupSparkSession(
                 argList,
-                "AverageAggregatorByYear",
-                "averages"
+                "StdevAggregatorByYear",
+                "stdev"
         );
 
         String inventoryId = argList.remove(0);
@@ -35,47 +30,21 @@ public class StdevAggregatorByYear {
         Util.logFilePaths(session, argList);
 
         try {
-            JavaRDD<Row> lines = session.read().format("csv")
+            Dataset<Row> lines = session.read().format("csv")
                     .option("sep", ",")
                     .option("inferSchema", "true")
                     .option("header", "true")
-                    .load(argList.stream().filter(it -> it.startsWith("s3a")).toArray(String[]::new))
-                    .toJavaRDD();
+                    .load(argList.stream().filter(it -> it.startsWith("s3a")).toArray(String[]::new));
 
-            // Group by year
-            JavaPairRDD<Integer, Iterable<Row>> byYear = lines.groupBy(row -> {
-                String dateString = row.getAs("Date");
-                return LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd")).getYear();
-            });
 
-            JavaPairRDD<Integer, Double> meanTempByYear = byYear.aggregateByKey(new Tuple2<>(0, 0.0),
-                    (tuple, rows) -> {
-                        AtomicReference<Integer> count = new AtomicReference<>(0);
-                        AtomicReference<Double> sum = new AtomicReference<>(0.0);
-                        rows.iterator().forEachRemaining(row -> {
-                            Double current = row.getAs(columnName);
-                            sum.updateAndGet(v -> v + current);
-                            count.updateAndGet(v -> v + 1);
-                        });
-                        return new Tuple2<>(count.get(), sum.get());
-                    },
-                    (v1, v2) -> new Tuple2<>(v1._1 + v2._1, v1._2 + v2._2)
-            ).mapValues(sum -> (1.0 * sum._2 / sum._1));
-
-            // Map results to
-            JavaRDD<Document> documents = meanTempByYear.map(tuple -> {
-                Map<String, String> valuesMap = Map.of(
-                        "inventoryId", inventoryId,
-                        "groupedBy", "year",
-                        "label", tuple._1.toString(),
-                        "avg", tuple._2.toString()
-                );
-                StringSubstitutor substitutor = new StringSubstitutor(valuesMap);
-                String templateString = "{ inventoryId: '${inventoryId}', year: ${year}, avg: ${avg} }";
-                return Document.parse(substitutor.replace(templateString));
-            });
-
-            MongoSpark.save(documents);
+            MongoSpark.save(lines
+                    .groupBy(
+                            new Column("Year").as("year"),
+                            new Column("ID").as("inventoryId")
+                    )
+                    .agg(functions.stddev(columnName).as("stdev"))
+                    .orderBy("Year")
+            );
         } catch (Exception e) {
             session.log().error("Spark aggregation function threw an error. Listing args below: ");
             // Remove aws credentials
